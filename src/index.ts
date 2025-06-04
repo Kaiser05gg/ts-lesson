f3c29fce93272f539abcb0052f3acb2b8ca872ce
Kaiser05gg <shu.iss1121@icloud.com>
2025-06-04 22:42:04 +0900

    1commit

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-const hello: string = "hello world";
-console.log(hello);
+// let strOrnumber: string | number = "hoge";
+// if (typeof strOrnumber === "number") {
+//   console.log("strOrNumber は数字型です");
+// }
+
+const user = {
+  id:,1
+  name:"hajime"
+  age:32
+  e-mail: adsfdsfzsdfew.com 
+}
